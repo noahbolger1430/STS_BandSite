50a632be7878db500ec0027b64d20e7de5d6359a
Noah Bolger <noahbolger@gmail.com>
2017-09-11 22:18:17 -0400

    First commit

---
+++ music.php
@@ -0,0 +1,40 @@
+<?php
+	include 'banner.html';
+?>
+<style>
+.music{
+	background: #CDBD9B;
+}
+.paragraph{
+	text-align: center;
+	color: white;
+	font-size: 18;
+}
+.music-title{
+	padding-top: 10;
+	color: white;
+	font-family: 'Homemade Apple', cursive;
+	text-align: center;
+}
+iframe{
+	display: block;
+	margin: 0 auto;
+	height: 380;
+}
+</style>
+
+<div class="music">
+	<div class="music-title">
+		<h1> Music </h1>
+	</div>
+
+	<hr>
+
+	<div class="paragraph">
+		<p>We take inspiration from many genres, artists, and styles. This spotify playlist includes some of our favourite songs, including songs we cover, and some that may be coming in the future.
+		</p>
+	</div>
+
+	<iframe class="spotify-playlist" src="https://open.spotify.com/embed?uri=spotify:user:22fa7j75ujseajnztfqbpvbdq:playlist:4hNAHhWw197hyaC5uhF6pv" frameborder="0" allowtransparency="true" align="center"></iframe>
+
+</div>
