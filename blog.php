3ba359b55823c0a2bec574091015621d4be74131
Noah Bolger <noahbolger@gmail.com>
2017-10-09 14:15:54 -0400

    add facebook widget to blog page

--- blog.php
+++ blog.php
@@ -56,6 +56,14 @@
 	  </div>
 	</nav>
 	<body>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 		<div class="blog">
 			<h1 class="title">Blog</h1>
 		</div>
@@ -67,6 +75,8 @@
 		  <input type="textarea" name="body" class="form-control input-lg"><br><br>
 		  <input type="submit" value="Submit">
 		</form>
+
+		<div class="fb-page" data-href="https://www.facebook.com/Smalltownstripclub/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Smalltownstripclub/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Smalltownstripclub/">Small-Town Strip Club</a></blockquote></div>
 
 	</body>
 </html>
