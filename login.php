0ff8286b3e5665fab7cbbbb7d8e60bfd9cadf9ee
Noah Bolger <noahbolger@gmail.com>
2017-10-14 21:02:15 -0400

    rewrite simpler login form with SESSIONS

--- login.php
+++ login.php
@@ -1,121 +1,33 @@
 <?php
-   ob_start();
-   session_start();
+   session_start(); // Starting Session
+   $error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+   if (empty($_POST['username']) || empty($_POST['password'])) {
+      $error = "Username or Password is invalid";
+   }
+   else {
+      // Define $username and $password
+      $username=$_POST['username'];
+      $password=$_POST['password'];
+      // Establishing Connection with Server by passing server_name, user_id and password as a parameter
+      $connection = mysql_connect("localhost", "root", "");
+      // To protect MySQL injection for Security purpose
+      $username = stripslashes($username);
+      $password = stripslashes($password);
+      $username = mysql_real_escape_string($username);
+      $password = mysql_real_escape_string($password);
+      // Selecting Database
+      $db = mysql_select_db("user_info", $connection);
+      // SQL query to fetch information of registerd users and finds user match.
+      $query = mysql_query("select * from user_info where password='$password' AND username='$username'", $connection);
+      $rows = mysql_num_rows($query);
+      if ($rows == 1) {
+         $_SESSION['login_user']=$username; // Initializing Session
+         header("location: banner.html"); // Redirecting To Other Page
+      } else {
+         $error = "Username or Password is invalid";
+      }
+      mysql_close($connection); // Closing Connection
+   }
+}
 ?>
-
-<?
-   // error_reporting(E_ALL);
-   // ini_set("display_errors", 1);
-?>
-
-<html lang = "en">
-   
-   <head>
-      <title>Tutorialspoint.com</title>
-      <link href = "css/bootstrap.min.css" rel = "stylesheet">
-      
-      <style>
-         body {
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #ADABAB;
-         }
-         
-         .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
-            color: #017572;
-         }
-         
-         .form-signin .form-signin-heading,
-         .form-signin .checkbox {
-            margin-bottom: 10px;
-         }
-         
-         .form-signin .checkbox {
-            font-weight: normal;
-         }
-         
-         .form-signin .form-control {
-            position: relative;
-            height: auto;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-            padding: 10px;
-            font-size: 16px;
-         }
-         
-         .form-signin .form-control:focus {
-            z-index: 2;
-         }
-         
-         .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-            border-color:#017572;
-         }
-         
-         .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-            border-color:#017572;
-         }
-         
-         h2{
-            text-align: center;
-            color: #017572;
-         }
-      </style>
-      
-   </head>
-	
-   <body>
-      
-      <h2>Enter Username and Password</h2> 
-      <div class = "container form-signin">
-         
-         <?php
-            $msg = '';
-            
-            if (isset($_POST['login']) && !empty($_POST['username']) 
-               && !empty($_POST['password'])) {
-				
-               if ($_POST['username'] == 'tutorialspoint' && 
-                  $_POST['password'] == '1234') {
-                  $_SESSION['valid'] = true;
-                  $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
-                  
-                  echo 'You have entered valid use name and password';
-               }else {
-                  $msg = 'Wrong username or password';
-               }
-            }
-         ?>
-      </div> <!-- /container -->
-      
-      <div class = "container">
-      
-         <form class = "form-signin" role = "form" 
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
-            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-            <input type = "text" class = "form-control" 
-               name = "username" placeholder = "username = tutorialspoint" 
-               required autofocus></br>
-            <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = 1234" required>
-            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
-               name = "login">Login</button>
-         </form>
-			
-         Click here to clean <a href = "logout.php" tite = "Logout">Session.
-         
-      </div> 
-      
-   </body>
-</html>
